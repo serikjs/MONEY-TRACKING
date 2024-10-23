beaf5f1241781c9defaecf5d8209fa708ad3f2fb
Sergey Skorokhod <serik.plus.plus@gmail.com>
2024-10-23 15:17:34 +0300

    update check login

--- src/App.vue
+++ src/App.vue
@@ -5,20 +5,13 @@
 
 <script setup>
 import { useAuthStore } from '@/stores/auth'
-import router from '@/router/index'
 import { supabase } from '@/lib/supabaseClient.js'
-import { onMounted, onUpdated } from 'vue'
-import Toasts from '@/components/common/ToastsDefault.vue'
 import ToastsDefault from "@/components/common/ToastsDefault.vue";
 
 const authStore = useAuthStore()
 
-onUpdated(() => {
-  authStore.checkLogin()
-})
-onMounted(() => {
-  authStore.checkLogin()
-
-})
+supabase.auth.onAuthStateChange( (event, session) => {
+   authStore.checkLogin()
+});
 </script>
 <style scoped></style>
