170186d41b1f787f3fef9aba5160a3e6b1cfb88e
Sergey Skorokhod <serik.plus.plus@gmail.com>
2024-10-24 14:58:22 +0300

    tables  types

---
+++ src/store_types/tables.types.ts
@@ -0,0 +1,5 @@
+
+export interface Tables {
+  income: string|null;
+  taxes: string|null;
+}
--- src/stores/tables.ts
+++ src/stores/tables.ts
@@ -1,19 +1,18 @@
 import { ref, computed } from 'vue'
 import { defineStore } from 'pinia'
 import {supabase} from "@/lib/supabaseClient.js";
-import { GoogleSpreadsheet } from "google-spreadsheet";
-import { useAuthStore } from "@/stores/auth";
+import type { Tables } from "@/store_types/tables.types";
 
 export const useTablesStore = defineStore('tables', () => {
-  const tables = ref( null )
-
-  const authStore = useAuthStore()
-
+  const tables = ref<Tables>( {
+    income: null,
+    taxes: null,
+  } )
   const getTables = computed(() => tables.value)
 
 
   function setTables(val = null) {
-    tables.value = val
+    // tables.value = val
   }
 
   async function uploadTables() {
@@ -21,26 +20,5 @@ export const useTablesStore = defineStore('tables', () => {
     setTables(data)
   }
 
-  async function uploadSheet(spreadsheetId:string) {
-    const token = authStore.getSession?.provider_token
-
-    if (!token) {
-      console.error('Токен отсутствует. Убедитесь, что пользователь авторизован.')
-      return
-    }
-
-    try {
-      const doc = new GoogleSpreadsheet(spreadsheetId, {
-        token
-      })
-
-      await doc.loadInfo()
-
-      return doc
-    } catch (error) {
-      console.error('Ошибка при доступе к Google Sheets:', error)
-    }
-  }
-
-  return { getTables, uploadTables,uploadSheet }
+  return { getTables, uploadTables }
 })
